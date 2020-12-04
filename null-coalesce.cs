81e25e92dbdfe5a97a2cc7ef1600a8f893ca5a93
cordis-dev <darius@adroiti.com>
2020-12-04 12:28:51 +0200

    Update null-coalesce.cs

--- null-coalesce.cs
+++ null-coalesce.cs
@@ -11,35 +11,10 @@ namespace System.Management.Automation
 {
     internal static class MshLog
     {
-        /// <summary>
-        /// LogCommandLifecycleEvent: Log a command lifecycle event.
-        ///
-        /// This is the only form of CommandLifecycleEvent logging api.
-        /// </summary>
-        /// <param name="executionContext">Execution Context for the current running engine.</param>
-        /// <param name="commandState">New command state.</param>
-        /// <param name="invocationInfo">Invocation data for current command that is running.</param>
         internal static void LogCommandLifecycleEvent(ExecutionContext executionContext,
                                                 CommandState commandState,
                                                 InvocationInfo invocationInfo)
         {
-            if (executionContext == null)
-            {
-                PSTraceSource.NewArgumentNullException(nameof(executionContext));
-                return;
-            }
-
-            if (invocationInfo == null)
-            {
-                PSTraceSource.NewArgumentNullException(nameof(invocationInfo));
-                return;
-            }
-
-            if (s_ignoredCommands.Contains(invocationInfo.MyCommand.Name))
-            {
-                return;
-            }
-
             LogContext logContext = null;
             foreach (LogProvider provider in GetLogProvider(executionContext))
             {
