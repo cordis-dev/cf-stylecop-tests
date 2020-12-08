fe3aee8df6c4c201e7113b81bf4f04889e664aef
Darius <darius@adroiti.com>
2020-12-08 09:52:48 +0200

    add Tests/csharp7/InParameters.cs

---
+++ Tests/csharp7/InParameters.cs
@@ -0,0 +1,12 @@
+﻿using System.Numerics;
+
+namespace Tests.csharp7
+{
+    public class InParameters
+    {
+        static Vector3 Add(in Vector3 v1, in Vector3 v2)
+        {
+            return new Vector3(v1.X + v2.X, v1.Y + v2.Y, v1.Z + v2.Z);
+        }
+    }
+}
