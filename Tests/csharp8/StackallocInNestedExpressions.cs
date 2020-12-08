968955bf78849a725eec7f36e99d4dd4e1c7cca3
Darius <darius@adroiti.com>
2020-12-08 10:44:07 +0200

    include classic stackalloc example

--- Tests/csharp8/StackallocInNestedExpressions.cs
+++ Tests/csharp8/StackallocInNestedExpressions.cs
@@ -6,7 +6,13 @@ namespace Tests
     {
         public void T()
         {
-            Span<int> numbers = stackalloc[] { 1, 2, 3, 4, 5, 6 };
+            // classic
+            unsafe
+            {
+                byte* tmp = stackalloc byte[10];
+            }
+
+            Span<int> numbers = stackalloc [] { 1, 2, 3, 4, 5, 6 };
             var ind = numbers.IndexOfAny(stackalloc[] { 2, 4, 6, 8 });
             Console.WriteLine(ind);  // output: 1
         }
