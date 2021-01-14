ac2c40ca4602d19e127cf28d36ee732a76e7e0ae
Darius <darius@adroiti.com>
2021-01-14 16:35:34 +0200

    update Tests/csharp8/SwitchExpressions.cs

--- Tests/csharp8/SwitchExpressions.cs
+++ Tests/csharp8/SwitchExpressions.cs
@@ -50,17 +50,19 @@ namespace Tests
 				Directions.Down  => Orientation.South,
 				Directions.Left  => Orientation.West,
 			};
+			
+			string errorReason = user.IsAbove(bot) switch
+            {
+                _ when isBot => "I wish I could kick myself, but I sadly cannot.",
+                _ => "Something has gone really wrong, and I don't know what *:(*"
+            };
 		}
 
         public static RGBColor FromRainbow(Rainbow colorBand) =>
             colorBand switch
             {
                 Rainbow.Red => new RGBColor(0xFF, 0x00, 0x00),
-                Rainbow.Orange => new RGBColor(0xFF, 0x7F, 0x00),
-                Rainbow.Yellow => new RGBColor(0xFF, 0xFF, 0x00),
-                Rainbow.Green => new RGBColor(0x00, 0xFF, 0x00),
-                Rainbow.Blue => new RGBColor(0x00, 0x00, 0xFF),
-                Rainbow.Indigo => new RGBColor(0x4B, 0x00, 0x82),
+                Rainbow.Orange or Rainbow.Blue => new RGBColor(0xFF, 0x7F, 0x00),
                 Rainbow.Violet => new RGBColor(0x94, 0x00, 0xD3),
                 _ => throw new ArgumentException(message: "invalid enum value", paramName: nameof(colorBand)),
             };
